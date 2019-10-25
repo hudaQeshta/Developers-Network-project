ab81c158bb1fc58a845e71edbd923d83795f0478
HuDa Qeshta <Qeshta.w.huda@gmail.com>
2019-10-25 08:26:02 +0400

    all files Commit

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const app = express();
 
 // Connect Database
 connectDB();
-app.get("/", (req, res) => res.send("API running"));
+
 // Init Middleware
-//app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }));
 
 // Define Routes
-//app.use("/api/users", require("./routes/api/users"));
-//app.use("/api/auth", require("./routes/api/auth"));
-//app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
 //app.use("/api/posts", require("./routes/api/posts"));
 
 // Serve static assets in production
