9d64b0b368704a3186f72dd42f986c85fb169d81
HuDa Qeshta <Qeshta.w.huda@gmail.com>
2020-09-17 09:45:10 +0400

    Prepared for final deployment

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,6 +1,5 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
